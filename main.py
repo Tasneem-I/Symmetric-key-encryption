7e482cc85b717622d03ee204e814344bcb1b4778
Tasneem <107906533+Tasneem-I@users.noreply.github.com>
2022-09-14 09:01:12 +0000

    Instead of using fernet I used pyscrypt which is more secure in generating keys

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
+import base64
+import os
 from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+import pyscrypt
+#informing the user on what the program does
+print("This is a password to key generator")
+print("Enter a password below ")
 
-string = str(input()) 
+#taking the input and converring it to bytes and storing it in a variable called password
+password= input().encode()
 
-print("your input: " , string)
+print("Wait for a few seconds, it takes a while to generate key.") 
+print("We apologise for the inconvenience")
+#generating a salt of 16 bytes using the urandom function and storing it in a variable
+salt = os.urandom(16) 
 
-key = Fernet.generate_key()
 
-fernet = Fernet(key)
+#using pyscrypt to generate a key. Pyscrypt is used here instead of pbkdf2 because pbkdf2 is not resistant to gpu and asic attacks
 
-enc = fernet.encrypt(string.encode())
+scrypt_key = pyscrypt.hash(
+  password=password,
+  salt= salt, 
+  N= 1024,
+  r= 8,
+  p=1, 
+  dkLen= 32
+                          )
 
-print("encrypted input: " , enc)
+# making sure the key is url and file system safe by using a function from base64
 
-dec = fernet.decrypt(enc).decode()
+key = base64.urlsafe_b64encode(scrypt_key)
 
-print("decrypted input: " , dec)
+print("Here's your key")
+print(key)
+
+
+
