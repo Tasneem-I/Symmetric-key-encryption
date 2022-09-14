5abc5aa2a08d15458e8fe86f63d10269ee0a267b
Tasneem-I <107906533+Tasneem-I@users.noreply.github.com>
2022-09-14 18:28:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,36 +1,52 @@
+import tkinter as tk
 import base64
 import os
 import pyscrypt
-#informing the user on what the program does
-print("This is a password to key generator")
-print("Enter a password below ")
 
-#taking the input and converring it to bytes and storing it in a variable called password
-password= input().encode()
+root = tk.Tk()
+root.title("Password Encryptor")
 
-print("Wait for a few seconds, it takes a while to generate key.") 
-print("We apologise for the inconvenience")
-#generating a salt of 16 bytes using the urandom function and storing it in a variable
-salt = os.urandom(16) 
+def submit():
+    lab2= Label(root, text="Your password is being encrypted. Do you wish to see the key?", state=ENABLED)
+    btn3= Button(root, text="Yes", command= yes(), state=ENABLED)
+    btn4= Button(root, text="No", command= no(), state= ENABLED) 
+    pwd= e1.get()
+    pwd= pwd.encode()
+    salt = os.urandom(16) 
+    scrypt_key = pyscrypt.hash(
+      password=password,
+      salt= salt, 
+      N= 1024,
+      r= 8,
+      p=1, 
+      dkLen= 32)
+    key = base64.urlsafe_b64encode(scrypt_key)
+    global key
+
+def clr_pwd():
+    e1.delete(0, END)
 
 
-#using pyscrypt to generate a key. Pyscrypt is used here instead of pbkdf2 because pbkdf2 is not resistant to gpu and asic attacks
+def clr_key_and_pwd():
+    e1.delete(0, END)
+    e2.delete(0, END)
 
-scrypt_key = pyscrypt.hash(
-  password=password,
-  salt= salt, 
-  N= 1024,
-  r= 8,
-  p=1, 
-  dkLen= 32
-                          )
 
-# making sure the key is url and file system safe by using a function from base64
+def yes():
+    lab2= Label(root, text="Your password is being encrypted. Do you wish to see the key?", state=DISABLED)
+    btn3= Button(root, text="Yes", command= yes(), state=DISABLED)
+    btn4= Button(root, text="No", command= no(), state= DISABLED) 
+    lab3= Label(root, text="Here's your key")
+    e2= Entry(root, width= 50px, borderwidth= 5px, state=ENABLED)
+    btn5 = Button(root, text="Clear All", command= clr_key_and_pwd(), state= ENABLED)
+    e2.insert(key)
 
-key = base64.urlsafe_b64encode(scrypt_key)
 
-print("Here's your key")
-print(key)
+def no():
+    lab2= Label(root, text="Your password is being encrypted. Do you wish to see the key?", state=DISABLED)
+    btn3= Button(root, text="Yes", command= yes(), state=DISABLED)
+    btn4= Button(root, text="No", command= no(), state= DISABLED)
+    
 
 
 
